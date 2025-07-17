dfe804576d8916ea53ba33399544864a7da26ee3
NhatCao <anhnhat22102003@gmail.com>
2025-07-17 19:00:35 +0700

    initial commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { assets } from '@/assets/assets';
+import React from 'react';
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <img src={assets.logo} alt="logo" className='h-8' />
+        </div>
+
+    )
+}
+
+export default Navbar
