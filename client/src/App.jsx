5730ed8e21e6b4983b240b823a4822c065a6c9ae
NhatCao <anhnhat22102003@gmail.com>
2025-07-17 23:38:33 +0700

    feat(navbar): create responsive navbar with links and logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
   return (
-    <div className='text-red-500'>App</div>
+    <>
+      <Navbar setShowLogin={setShowLogin} />
+    </>
   )
 }
 
