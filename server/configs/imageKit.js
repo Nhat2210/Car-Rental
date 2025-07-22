1291c37b72e2c19734fda4b43823b9e081145202
NhatCao <anhnhat22102003@gmail.com>
2025-07-22 15:30:16 +0700

    feat: conect Imagekit for image storage

---
+++ server/configs/imageKit.js
@@ -0,0 +1,12 @@
+import ImageKit from "imagekit";
+
+// or
+
+
+var imagekit = new ImageKit({
+    publicKey: process.env.IMAGE_PUBLIC_KEY,
+    privateKey: process.env.IMAGE_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGE_URL_ENDPOINT
+});
+
+export default imagekit;
