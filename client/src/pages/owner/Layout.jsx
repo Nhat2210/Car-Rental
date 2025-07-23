9bb89f6cba08c86f19783e0c2b5c541f83f83826
NhatCao <anhnhat22102003@gmail.com>
2025-07-23 13:56:52 +0700

    feat(owner): fetch Dashboard Data

--- client/src/pages/owner/Layout.jsx
+++ client/src/pages/owner/Layout.jsx
@@ -1,18 +1,27 @@
-import NavbarOwner from '@/components/owner/NavbarOwner'
-import Sidebar from '@/components/owner/Sidebar'
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import NavbarOwner from "@/components/owner/NavbarOwner";
+import Sidebar from "@/components/owner/Sidebar";
+import { useAppContext } from "@/context/AppContext";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 
 const Layout = () => {
-    return (
-        <div className='flex flex-col'>
-            <NavbarOwner />
-            <div className='flex'>
-                <Sidebar />
-                <Outlet />
-            </div>
-        </div>
-    )
-}
+  const { isOwner, navigate } = useAppContext();
 
-export default Layout
+  useEffect(() => {
+    if (!isOwner) {
+      navigate("/");
+    }
+  }, []);
+  return (
+    <div className="flex flex-col">
+      <NavbarOwner />
+      <div className="flex">
+        <Sidebar />
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
