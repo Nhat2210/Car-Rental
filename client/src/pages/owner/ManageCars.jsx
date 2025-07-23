ad56ab48f03aa4f1e952011a654298f9317dfa3e
NhatCao <anhnhat22102003@gmail.com>
2025-07-23 23:27:35 +0700

    feat(owner): add car management API service with CRUD operations

--- client/src/pages/owner/ManageCars.jsx
+++ client/src/pages/owner/ManageCars.jsx
@@ -1,67 +1,141 @@
-import { assets, dummyCarData } from '@/assets/assets';
-import Title from '@/components/Title'
-import React, { useEffect, useState } from 'react'
+import { assets } from "@/assets/assets";
+import Title from "@/components/Title";
+import { useAppContext } from "@/context/AppContext";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const ManageCars = () => {
+  const { isOwner, axios, navigate, currency } = useAppContext();
 
-    const [cars, setCars] = useState([]);
-    const currency = import.meta.env.VITE_CURRENCY
+  const [cars, setCars] = useState([]);
 
-
-    const fetchOwnerCars = async () => {
-        setCars(dummyCarData);
-        console.log(cars);
-
+  const fetchOwnerCars = async () => {
+    try {
+      const { data } = await axios.get("/api/owner/cars");
+      console.log(data);
+      if (data.success) {
+        setCars(data.cars);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-    useEffect(() => {
+  const toggleAvailability = async (carId) => {
+    try {
+      const { data } = await axios.post("/api/owner/toggle-car", { carId });
+      console.log(data);
+      if (data.success) {
+        toast.success(data.message);
         fetchOwnerCars();
-    }, [])
-    return (
-        <div className='px-4 pt-10 md:px-10 w-full'>
-            <Title title="Manage Cars" subTitle="View all listed cars, update their details, or remove them from the booking platform" align="left" />
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
-            <div className='max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6'>
-                <table className='w-full border-collapse text-left text-sm text-gray-600'>
-                    <thead>
-                        <tr>
-                            <th className='p-3 font-medium'>Car</th>
-                            <th className='p-3 font-medium'>Category</th>
-                            <th className='p-3 font-medium'>Price</th>
-                            <th className='p-3 font-medium'>Status</th>
-                            <th className='p-3 font-medium'>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {cars.map((car, index) => (
-                            <tr key={index} className='border-t border-borderColor'>
-                                <td className='p-3 flex items-center gap-3'>
-                                    <img src={car.image} alt="" className='w-12 h-12 aspect-square rounded-md object-cover' />
-                                    <div className='max-md:hidden'>
-                                        <p className='font-medium'>{car.brand} {car.model}</p>
-                                        <p className='font-medium'>{car.seating_capacity} seats • {car.transmission}</p>
-                                    </div>
-                                </td>
-                                <td className='p-3 max-md:hidden'>
-                                    {car.category}
-                                </td>
-                                <td className='p-3'>
-                                    {currency} {car.pricePerDay} /day
-                                </td>
-                                <td className='p-3 max-md:hidden'>
-                                    <span className={`px-3 py-1 rounded-full text-xs ${car.isAvaliable ? "bg-green-100 text-green-500" : "bg-red-100 text-red-500"}`}>{car.isAvaliable ? "Available" : "Unavailable"}</span>
-                                </td>
-                                <td className='flex items-center p-3'>
-                                    <img src={car.isAvaliable ? assets.eye_close_icon : assets.edit_icon} alt="" className='cursor-pointer' />
-                                    <img src={assets.delete_icon} alt="" className='cursor-pointer' />
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
-}
+  const deleteCar = async (carId) => {
+    try {
+      const confirm = window.confirm(
+        "Are you sure you want to delete this car? "
+      );
+      if (!confirm) return null;
 
-export default ManageCars
+      const { data } = await axios.post("/api/owner/delete-car", { carId });
+      console.log(data);
+      if (data.success) {
+        toast.success(data.message);
+        fetchOwnerCars();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    isOwner && fetchOwnerCars();
+  }, [isOwner]);
+  return (
+    <div className="px-4 pt-10 md:px-10 w-full">
+      <Title
+        title="Manage Cars"
+        subTitle="View all listed cars, update their details, or remove them from the booking platform"
+        align="left"
+      />
+
+      <div className="max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6">
+        <table className="w-full border-collapse text-left text-sm text-gray-600">
+          <thead>
+            <tr>
+              <th className="p-3 font-medium">Car</th>
+              <th className="p-3 font-medium">Category</th>
+              <th className="p-3 font-medium">Price</th>
+              <th className="p-3 font-medium">Status</th>
+              <th className="p-3 font-medium">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cars.map((car, index) => (
+              <tr key={index} className="border-t border-borderColor">
+                <td className="p-3 flex items-center gap-3">
+                  <img
+                    src={car.image}
+                    alt=""
+                    className="w-12 h-12 aspect-square rounded-md object-cover"
+                  />
+                  <div className="max-md:hidden">
+                    <p className="font-medium">
+                      {car.brand} {car.model}
+                    </p>
+                    <p className="font-medium">
+                      {car.seating_capacity} seats • {car.transmission}
+                    </p>
+                  </div>
+                </td>
+                <td className="p-3 max-md:hidden">{car.category}</td>
+                <td className="p-3">
+                  {currency} {car.pricePerDay} /day
+                </td>
+                <td className="p-3 max-md:hidden">
+                  <span
+                    className={`px-3 py-1 rounded-full text-xs ${
+                      car.isAvaliable
+                        ? "bg-green-100 text-green-500"
+                        : "bg-red-100 text-red-500"
+                    }`}
+                  >
+                    {car.isAvaliable ? "Available" : "Unavailable"}
+                  </span>
+                </td>
+                <td className="flex items-center p-3">
+                  <img
+                    onClick={() => toggleAvailability(car._id)}
+                    src={
+                      car.isAvaliable ? assets.eye_close_icon : assets.eye_icon
+                    }
+                    alt=""
+                    className="cursor-pointer"
+                  />
+                  <img
+                    onClick={() => deleteCar(car._id)}
+                    src={assets.delete_icon}
+                    alt=""
+                    className="cursor-pointer"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ManageCars;
--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -2,188 +2,188 @@ import { log } from "console";
 import imagekit from "../configs/imageKit.js";
 import Car from "../models/Car.js";
 import User from "../models/User.js";
-import fs from 'fs'
+import fs from "fs";
 import Booking from "../models/Booking.js";
 
-
 export const changeRoleToOwner = async (req, res) => {
-    try {
-        const { _id } = req.user;
-        await User.findByIdAndUpdate(_id, { role: "owner" })
-        res.json({ success: true, message: "Now you can list cars" })
-    } catch (error) {
-        console.log(error.message);
-        res.json({ success: false, message: "Now you can list cars" })
-    }
-}
-
+  try {
+    const { _id } = req.user;
+    await User.findByIdAndUpdate(_id, { role: "owner" });
+    res.json({ success: true, message: "Now you can list cars" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: "Now you can list cars" });
+  }
+};
 
 // APi to List Car
 
-
 export const addCar = async (req, res) => {
-    try {
-        const { _id } = req.user;
-        let car = JSON.parse(req.body.carData);
-        const imageFile = req.file;
+  try {
+    const { _id } = req.user;
+    let car = JSON.parse(req.body.carData);
+    const imageFile = req.file;
 
-        // upload image to imagekit
-        const fileBuffer = fs.readFileSync(imageFile.path)
-        const response = await imagekit.upload({
-            file: fileBuffer,
-            fileName: imageFile.originalname,
-            folder: '/cars'
-        })
+    // upload image to imagekit
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/cars",
+    });
 
-        // Optimization through imageKit URL transformation
-        var optimizedImageUrl = imagekit.url({
-            path: response.filePath,
-            transformation: [
-                { width: '1280' }, // width resizing
-                { quality: 'auto' }, // Auto Compression 
-                { format: 'webp' } // conver to modern format
-            ]
-        });
+    // Optimization through imageKit URL transformation
+    var optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { width: "1280" }, // width resizing
+        { quality: "auto" }, // Auto Compression
+        { format: "webp" }, // conver to modern format
+      ],
+    });
 
-        const image = optimizedImageUrl;
-        await Car.create({ ...car, owner: _id, image });
+    const image = optimizedImageUrl;
+    await Car.create({ ...car, owner: _id, image });
 
-        res.json({ success: true, message: "Car Added" })
-    } catch (error) {
-        console.log(error.message);
-        res.json({ success: false, message: error.message })
-
-    }
-}
-
+    res.json({ success: true, message: "Car Added" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // API to Lít Owner Cars
 
 export const getOwnerCars = async (req, res) => {
-    try {
-        const { _id } = req.user;
-        const cars = await Car.find({ owner: _id })
-        res.json({ success: true, cars })
-    } catch (error) {
-        console.log(error.message);
-        res.json({ success: false, message: error.message })
-
-    }
-}
+  try {
+    const { _id } = req.user;
+    const cars = await Car.find({ owner: _id });
+    res.json({ success: true, cars });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // API to Toggle Car Availability
 
 export const toggleCarAvailability = async (req, res) => {
-    try {
-        const { _id } = req.user;
-        const { carId } = req.body;
-        const car = await Car.findById({ owner: _id })
+  try {
+    const { _id } = req.user;
+    const { carId } = req.body;
+    const car = await Car.findById(carId);
 
-        // Checking is car belongs to the user 
-        if (car.owner.toString() !== _id.toString()) {
-            return res.json({ success: false, message: "Unauthorized" });
-        }
-        car.isAvaliable = !car.isAvaliable;
-        await car.save();
-
-        res.json({ success: true, message: "Avalibility Toggled" })
-    } catch (error) {
-        console.log(error.message);
-        res.json({ success: false, message: error.message })
-
+    // Checking is car belongs to the user
+    if (car.owner.toString() !== _id.toString()) {
+      return res.json({ success: false, message: "Unauthorized" });
     }
-}
+    car.isAvaliable = !car.isAvaliable;
+    await car.save();
 
+    res.json({ success: true, message: "Avalibility Toggled" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // API delete car
 export const deleteCar = async (req, res) => {
-    try {
-        const { _id } = req.user;
-        const { carId } = req.body;
-        const car = await Car.findById(carId)
+  try {
+    const { _id } = req.user;
+    const { carId } = req.body;
+    const car = await Car.findById(carId);
 
-        //checking car belongs to the user
-        if (car.owner.toString() !== _id.toString()) {
-            return res.json({ success: false, message: "Unauthorized" });
-        }
-
-        car.owner = null;
-        car.isAvaliable = false;
-
-        res.json({ success: true, message: "Car Removed" })
-    } catch (error) {
-        console.log(error.message);
-        res.json({ success: false, message: error.message })
-
+    //checking car belongs to the user
+    if (car.owner.toString() !== _id.toString()) {
+      return res.json({ success: false, message: "Unauthorized" });
     }
-}
+
+    car.owner = null;
+    car.isAvaliable = false;
+
+    await car.save();
+
+    res.json({ success: true, message: "Car Removed" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // API to get Dashboard Data
 
 export const getDashboardData = async (req, res) => {
-    try {
-        const { _id, role } = req.user;
+  try {
+    const { _id, role } = req.user;
 
-        if (role !== 'owner') {
-            return res.json({ success: false, message: "Unauthorized" })
-        }
-        const cars = await Car.find({ owner: _id })
-        const bookings = await Booking.find({ owner: _id }).populate('car').sort({ createdAt: -1 })
-
-        const pendingBookings = await Booking.find({ owner: _id, status: "pending" })
-        const completedBookings = await Booking.find({ owner: _id, status: "confirmed" })
-
-        // Caculate monthlyRevenue from bookings where status is confirmed
-
-        const monthlyRevenue = bookings.slice().filter(booking => booking.status === 'confirmed').reduce((acc, booking) => acc + booking.price, 0)
-
-        const dashboardData = {
-            totalCars: cars.length,
-            totalBookings: bookings.length,
-            pendingBookings: pendingBookings.length,
-            completedBookings: completedBookings.length,
-            recentBookings: bookings.splice(0, 3),
-            monthlyRevenue
-        }
-
-        res.json({ success: true, dashboardData })
-    } catch (error) {
-        console.log(error.message);
-        res.json({ success: false, message: error.message })
-
+    if (role !== "owner") {
+      return res.json({ success: false, message: "Unauthorized" });
     }
-}
+    const cars = await Car.find({ owner: _id });
+    const bookings = await Booking.find({ owner: _id })
+      .populate("car")
+      .sort({ createdAt: -1 });
 
+    const pendingBookings = await Booking.find({
+      owner: _id,
+      status: "pending",
+    });
+    const completedBookings = await Booking.find({
+      owner: _id,
+      status: "confirmed",
+    });
+
+    // Caculate monthlyRevenue from bookings where status is confirmed
+
+    const monthlyRevenue = bookings
+      .slice()
+      .filter((booking) => booking.status === "confirmed")
+      .reduce((acc, booking) => acc + booking.price, 0);
+
+    const dashboardData = {
+      totalCars: cars.length,
+      totalBookings: bookings.length,
+      pendingBookings: pendingBookings.length,
+      completedBookings: completedBookings.length,
+      recentBookings: bookings.splice(0, 3),
+      monthlyRevenue,
+    };
+
+    res.json({ success: true, dashboardData });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // API to update user image
 export const updateUserImage = async (req, res) => {
-    try {
-        const { _id } = req.user;
-        const imageFile = req.file;
+  try {
+    const { _id } = req.user;
+    const imageFile = req.file;
 
-        // Upload Image to ImageKit
-        const fileBuffer = fs.readFileSync(imageFile.path)
-        const response = await imagekit.upload({
-            file: fileBuffer,
-            fileName: imageFile.originalname,
-            folder: '/cars'
-        })
-        // Optimization through imageKit URL transformation
-        var optimizedImageUrl = imagekit.url({
-            path: response.filePath,
-            transformation: [
-                { width: '400' }, // width resizing
-                { quality: 'auto' }, // Auto Compression 
-                { format: 'webp' } // conver to modern format
-            ]
-        });
+    // Upload Image to ImageKit
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/cars",
+    });
+    // Optimization through imageKit URL transformation
+    var optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { width: "400" }, // width resizing
+        { quality: "auto" }, // Auto Compression
+        { format: "webp" }, // conver to modern format
+      ],
+    });
 
-        const image = optimizedImageUrl;
+    const image = optimizedImageUrl;
 
-        await User.findByIdAndUpdate(_id, { image });
+    await User.findByIdAndUpdate(_id, { image });
 
-        res.json({ success: true, message: "Image Updated" })
-    } catch (error) {
-
-    }
-}
+    res.json({ success: true, message: "Image Updated" });
+  } catch (error) {}
+};
